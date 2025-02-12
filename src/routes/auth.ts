d1a1e030ab0437d185daa8b3bd981ddc1112b107
devMukulSingh <mukulsingh2276@gmail.com>
2025-02-12 16:55:42 +0530

    added login controller

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,7 +9,6 @@ const authApp = new Hono();
 authApp.post('/register', async (c) => {
     try {
         const body = await c.req.json();
-        console.log(body);
         const parsedBody = authSchema.safeParse(body);
         if (!parsedBody.success) {
             return c.json({
@@ -45,7 +44,9 @@ authApp.post('/register', async (c) => {
         return c.json({
             msg: "User created successfully",
             data: {
-                ...newUser,
+                id:newUser.id,
+                name:newUser.name,
+                email:newUser.email,
                 token
             }
         }, 201)
@@ -58,6 +59,55 @@ authApp.post('/register', async (c) => {
     }
 
 })
+//////////////////////////////////////////////////////////////////////////////
+authApp.post('/login', async (c) => {
+    try {
+        const body = await c.req.json();
+        const parsedBody = authSchema.omit({ name: true }).safeParse(body);
+        if (!parsedBody.success) {
+            return c.json({
+                "error": parsedBody.error.errors.map(err => err.message),
+            }, 400)
+        }
+        const { email, password } = parsedBody.data
 
+        const payload = {
+            exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24, 
+        }
+        const secret = process.env.JWT_SECRET;
+        const token = await sign(payload, secret as string)
+        const user = await prisma.user.findFirst({
+            where: {
+                email,
+            }
+        })
+        if (!user) {
+            return c.json({
+                error: "Invalid credentials",
+            }, 400)
+        }
+        if (!(await bcrypt.compare(password, user.password))) {
+            return c.json({
+                error: "Invalid credentials",
+            }, 400)
+        }
+        return c.json({
+            msg: "User loggedin successfully",
+            data: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                token
+            }
+        }, 200)
+    }
+    catch (e) {
+        console.log(e);
+        return c.json({
+            error: "Internal server error"
+        }, 500)
+    }
+
+})
 
 export default authApp;
